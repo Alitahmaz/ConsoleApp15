168318aff127bdde6ab54342a2e9ad86eac63428
Alitahmaz <alirt@code.edu.az>
2022-12-20 20:50:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 ci eded daxil edin:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("2 ci eded daxil edin:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check if one is negative and one is positive:");
+            Console.WriteLine((a < 0 && b > 0) || (a > 0 && b < 0));
+        }
+    }
+}
